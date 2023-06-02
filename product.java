54346aedf7773ef5bc2b8db1d40d37ce5be1f073
Shuvo Kumar Saha <65779943+Shuvo01912@users.noreply.github.com>
2023-06-02 22:12:01 +0600

    Assignment -01 java

---
+++ product.java
@@ -0,0 +1,19 @@
+
+package basicjava.basicjava;
+
+/**
+ *
+ * @author Shuvo Kumar Saha
+ */
+public class product {
+    public static void main(String[] agrs){
+    
+        System.out.println("id: 101");
+        System.out.println("title: iphone15");
+        System.out.println("price: 1895 euros");
+        System.out.println("description: perfect product with best image quality.");
+        System.out.println("category: phone");
+        
+    }
+    
+}
