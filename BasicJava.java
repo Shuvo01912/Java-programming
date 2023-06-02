ba9674e932caba0314607d4a8a0b6ad7f6ccb4fa
Shuvo Kumar Saha <65779943+Shuvo01912@users.noreply.github.com>
2023-06-02 22:03:54 +0600

    print on java

---
+++ BasicJava.java
@@ -0,0 +1,11 @@
+package basicjava.basicjava;
+
+public class BasicJava{
+    public static void main(String[] agrs){
+        System.out.println("Name: Shuvo Kumar Saha");
+        System.out.println("Dept: Student Of Computer Scicnce");
+        System.out.println("Class: 3rd Semester");
+    }
+    
+
+}
